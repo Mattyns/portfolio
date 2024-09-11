1275d6ee72f531e1b41a1d27f7f13073d1ceb4b2
Matheus Everton <81mme.matheus@gmail.com>
2024-09-11 17:13:05 -0300

    11/09/2024

---
+++ Script/script.js
@@ -0,0 +1,24 @@
+function mudarback () {
+    var back = document.getElementById("corpo").style.backgroundImage = 'url("img/pexels-johannes2.jpg")';
+}
+
+function voltarback () {
+    var back = document.getElementById("corpo").style.backgroundImage = 'url("img/pexels-sr-solo-2917871.jpg")';
+}
+
+function alternarback () {
+    var perfil = document.getElementById("perfil");
+    var back = document.getElementById("corpo").style.backgroundImage = 'url("img/pexels-johannes2.jpg")';
+    perfil.setAttribute("onclick", "alternarback2(this)");
+    perfil.setAttribute("onmouseover", "voltarback(this)");
+    perfil.setAttribute("onmouseout", "mudarback(this)");
+}
+
+function alternarback2 () {
+    var perfil = document.getElementById("perfil");
+    var back = document.getElementById("corpo").style.backgroundImage = 'url("img/pexels-sr-solo-2917871.jpg")';
+    perfil.setAttribute("onclick", "alternarback(this)");
+    perfil.setAttribute("onmouseover", "mudarback(this)");
+    perfil.setAttribute("onmouseout", "voltarback(this)");
+    
+}
